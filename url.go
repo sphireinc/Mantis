9128c5b5e178db70f3fe43726d41e35e0561dbd3
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2019-05-30 16:01:11 -0400

    Add UrlParameter Fetching function
    Signed-off-by: Juan Sanchez <juan.sanchez@juanleonardosanchez.com>

--- url.go
+++ url.go
@@ -14,7 +14,13 @@ func GetQueryParameter(r *http.Request, key string) []string {
 	return value
 }
 
-// GetQueryParameterFirst Returns the first element of the requested URL query parameter
+// GetQueryParameterFirst Returns the first element of the requested URL query parameter (ie /some/url?key=value)
 func GetQueryParameterFirst(r *http.Request, key string) string {
 	return GetQueryParameter(r, key)[0]
+}
+
+// GetUrlParameter Returns a dynamic url based parameter based on the key (ie /some/url/:parameter - key = 'parameter')
+func GetUrlParameter(r *http.Request, key string) string {
+	value := r.URL.Query().Get(":" + key)
+	return value
 }
