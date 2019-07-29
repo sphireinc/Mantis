bf664cb8d2ed03785d8f5925b3eb1707d39bf940
Sphire, Inc <47276851+sphireco@users.noreply.github.com>
2019-07-28 20:56:30 -0400

    Update error.go
    Expand these to use the mantis log wrapper functions rather than default log

--- error.go
+++ error.go
@@ -1,16 +1,23 @@
 package mantis
 
-import "log"
+var logger Log
 
-// Handle error
+// SetErrorLog Sets the logger interface to utilize to log
+func SetErrorLog(logr Log) {
+	logger = logr
+}
+
+// HandleError Handles an error with an error message
 func HandleError(message string, err error) {
 	if err != nil {
-		log.Println(message, err)
+		logger.Write(fmt.Sprintf("%s => %s", message, err.Error()))
 	}
 }
 
+// HandleFatalError Wrapper to panic
 func HandleFatalError(err error) {
 	if err != nil {
+		logger.Write(fmt.Sprintf("Fatal panic => %s", err.Error()))
 		panic(err)
 	}
 }
