16ee859d64a0e2bba8d0df24463f346b61504227
Juan Sanchez <jsanchez@shutterstock.com>
2019-11-26 17:17:09 -0500

    update url tests

--- url.go
+++ url.go
@@ -2,7 +2,6 @@ package mantis
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -24,32 +23,17 @@ func GetBody(r *http.Request) []byte {
 }
 
 // GetQueryParameter fetches a URL query parameter based on a key and return a string array
-func GetQueryParameter(r *http.Request, key string) []string {
+func GetQueryParameter(r *http.Request, key string) string {
 	value, ok := r.URL.Query()[key]
 	if !ok || len(value) < 1 {
-		return nil
+		return ""
 	}
-	return value
+	return value[0]
 }
 
-// GetQueryParameterFirst returns the first element of the requested URL query parameter (ie /some/url?key=value)
-func GetQueryParameterFirst(r *http.Request, key string) string {
-	return GetQueryParameter(r, key)[0]
-}
-
-// GetUrlParameter returns a dynamic url based parameter based on the key (ie /some/url/:parameter - key = 'parameter')
-func GetUrlParameter(r *http.Request, key string) string {
-	value := r.URL.Query().Get(key)
-	if value == "" {
-		vars := mux.Vars(r)
-		value = vars[key]
-	}
-	return value
-}
-
-// GetUrlParameters returns all url based parameters
-func GetUrlParameters(r *http.Request) map[string]string {
-	return mux.Vars(r)
+// GetQueryParameters returns all query parameters
+func GetQueryParameters(r *http.Request) url.Values {
+	return r.URL.Query()
 }
 
 // ParseUrl returns a *url.URL from a given URL string
--- url_test.go
+++ url_test.go
@@ -1,19 +1,155 @@
 package mantis
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type ReaderStruct struct {
+	A string
+	B string
+}
+
+var readerStruct = ReaderStruct{
+	A: "123",
+	B: "456",
+}
+
+var readerString, _ = json.Marshal(&readerStruct)
+
+func getRequestHelper(useReaderString bool) *http.Request {
+	w := httptest.NewRecorder()
+
+	var r *http.Request
+	if useReaderString {
+		r = httptest.NewRequest(http.MethodPost, "/some/test", strings.NewReader(string(readerString)))
+		r.Header.Set("Content-Type", "application/json")
+	} else {
+		r = httptest.NewRequest(http.MethodPost, "/?a=1&b=2", strings.NewReader("a=1&b=2"))
+		r.Header.Set("Content-Type", "application/x-www-urlencoded-form")
+	}
+
+	func(w http.ResponseWriter, r *http.Request) {
+		_ = r.ParseForm()
+	}(w, r)
+	return r
+}
+
+func TestParseBodyIntoStruct(t *testing.T) {
+	val := getRequestHelper(true)
+	rSVal := ReaderStruct{}
+
+	if err := ParseBodyIntoStruct(val, &rSVal); err != nil {
+		t.Errorf("ParseBodyIntoStruct Failed while reading: " + err.Error())
+	}
+
+	if rSVal.A != readerStruct.A {
+		t.Fatalf("expected '%s', got '%s'", rSVal.A, readerStruct.A)
+	}
+
+	if rSVal.B != readerStruct.B {
+		t.Fatalf("expected '%s', got '%s'", rSVal.B, readerStruct.B)
+	}
+}
+
+func TestGetBody(t *testing.T) {
+	val := getRequestHelper(true)
+	body := GetBody(val)
+
+	if bytes.Compare(body, readerString) == 1 {
+		t.Fatalf("expected '%s', got '%s'", string(body), string(readerString))
+	}
+}
 
 func TestGetQueryParameter(t *testing.T) {
+	val := getRequestHelper(false)
 
+	tests := []struct {
+		actual   string
+		expected string
+	}{
+		{GetQueryParameter(val, "a"), "1"},
+		{GetQueryParameter(val, "b"), "2"},
+		{GetQueryParameter(val, "z"), ""},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			if !reflect.DeepEqual(test.actual, test.expected) {
+				t.Fatalf("expected '%s', got '%s'", test.expected, test.actual)
+			}
+		})
+	}
 }
 
-func TestGetQueryParameterFirst(t *testing.T) {
+func TestGetQueryParameters(t *testing.T) {
+	val := getRequestHelper(false)
 
+	t1 := url.Values{}
+	t1.Set("a", "1")
+	t1.Set("b", "2")
+
+	tests := []struct {
+		actual   url.Values
+		expected url.Values
+	}{
+		{GetQueryParameters(val), t1 },
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			if !reflect.DeepEqual(test.actual, test.expected) {
+				t.Fatalf("expected '%s', got '%s'", test.expected, test.actual)
+			}
+		})
+	}
 }
 
-func TestGetUrlParameter(t *testing.T) {
+func TestParseUrl(t *testing.T) {
+	t1 := url.URL{
+		Scheme:     "",
+		Opaque:     "",
+		User:       nil,
+		Host:       "",
+		Path:       "",
+		RawPath:    "",
+		ForceQuery: false,
+		RawQuery:   "",
+		Fragment:   "",
+	}
+	
+	t2 := url.URL{
+		Scheme:     "https",
+		Opaque:     "",
+		User:       nil,
+		Host:       "google.com",
+		Path:       "/maps",
+		RawPath:    "",
+		ForceQuery: false,
+		RawQuery:   "v=1",
+		Fragment:   "",
+	}
 
-}
+	tests := []struct {
+		actual   *url.URL
+		expected *url.URL
+	}{
+		{ParseUrl(""), &t1},
+		{ParseUrl("https://google.com/maps?v=1"), &t2},
+	}
 
-func TestGetUrlParameters(t *testing.T) {
-
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			if !reflect.DeepEqual(test.actual, test.expected) {
+				t.Fatalf("expected '%s', got '%s'", test.expected, test.actual)
+			}
+		})
+	}
 }
