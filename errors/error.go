0f687d4e2dfc11b29586214c69207196c07d4c34
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2021-12-16 22:03:29 -0500

    update errors package

--- errors/error.go
+++ errors/error.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"time"
 )
 
 type Errors struct {
-	code    int32
-	message string
+	code      int32
+	message   string
+	timestamp time.Time
 }
 
 func (e *Errors) Code() int32 {
@@ -19,22 +21,32 @@ func (e *Errors) Message() string {
 	return e.message
 }
 
-// MarshalJSON implements the JSON encoding interface
-func (e *Errors) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]interface{}{
-		"code":    e.code,
-		"message": e.message,
-	})
-}
-
-func (e *Errors) ToError() error {
-	return errors.New(fmt.Sprintf("%v: %v", e.Code(), e.Message()))
+func (e *Errors) Time() string {
+	return e.timestamp.String()
 }
 
 // New creates a new errors instance
 func New(code int32, message string, args []any) *Errors {
 	if len(args) > 0 {
-		return &Errors{code, fmt.Sprintf(message, args...)}
+		message = fmt.Sprintf(message, args...)
 	}
-	return &Errors{code, message}
+	return &Errors{
+		code:      code,
+		message:   message,
+		timestamp: time.Now(),
+	}
+}
+
+// MarshalJSON implements the JSON encoding interface
+func (e *Errors) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"code":    e.code,
+		"message": e.message,
+		"time":    e.timestamp.String(),
+	})
+}
+
+// Marshal creates a Go STD error from our Errors object
+func (e *Errors) Marshal() error {
+	return errors.New(fmt.Sprintf("%v (error %v) %v", e.Time(), e.Code(), e.Message()))
 }
