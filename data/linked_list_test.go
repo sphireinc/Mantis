9c2521f2573740246804802615524e19a2f1655c
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2021-12-30 12:33:27 -0500

    update LL tests, remove heap.go as it's in STD

--- data/linked_list_test.go
+++ data/linked_list_test.go
@@ -5,29 +5,46 @@ import (
 	"testing"
 )
 
-func newLinkedListHelper() *list {
+func TestList_NewLinkedList(t *testing.T) {
 	l := NewLinkedList()
-	l.Insert("first key")
-	l.Insert("second key")
-	l.Insert("third key")
-	l.Insert("fourth key")
-	return l
+	l.Insert("first")
+	l.Insert("second")
+	l.Insert("third")
+	l.Insert("fourth")
+	assert.Equal(t, l.String(""), "fourth -> third -> second -> first")
 }
 
-func TestNewLinkedList(t *testing.T) {
-	l := newLinkedListHelper()
-	assert.Equal(t, l.String(""), "fourth key -> third key -> second key -> first key")
+func TestList_Pop(t *testing.T) {
+	l := NewLinkedList()
+	l.Insert("first")
+	l.Insert("second")
+	l.Insert("third")
+	l.Insert("fourth")
+	assert.Equal(t, l.String(""), "fourth -> third -> second -> first")
 	l.Pop()
-	assert.Equal(t, l.String(""), "fourth key -> third key -> second key")
+	assert.Equal(t, l.String(""), "fourth -> third -> second")
 	l.Pop()
-	assert.Equal(t, l.String(""), "fourth key -> third key")
+	assert.Equal(t, l.String(""), "fourth -> third")
 	l.Pop()
-	assert.Equal(t, l.String(""), "fourth key")
+	assert.Equal(t, l.String(""), "fourth")
 }
 
-func TestReverseLinkedList(t *testing.T) {
-	l := newLinkedListHelper()
-	assert.Equal(t, l.String(""), "fourth key -> third key -> second key -> first key")
+func TestList_Reverse(t *testing.T) {
+	l := NewLinkedList()
+	l.Insert("first")
+	l.Insert("second")
+	l.Insert("third")
+	l.Insert("fourth")
+	assert.Equal(t, l.String(""), "fourth -> third -> second -> first")
 	l.Reverse()
-	assert.Equal(t, l.String(""), "first key -> second key -> third key -> fourth key")
+	assert.Equal(t, l.String(""), "first -> second -> third -> fourth")
+}
+
+func TestList_Print(t *testing.T) {
+	s := NewLinkedList()
+	s.Insert(3)
+	s.Insert(5)
+	s.Insert(7)
+	s.Insert(9)
+	s.Print()
 }
