f796e3ad40aee7cee583aef60c38a98feebf4f72
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2022-04-13 15:23:46 -0400

    fix lint error

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -9,12 +9,14 @@ type UUID struct {
 	UUID guid.UUID `json:"uuid"`
 }
 
+// New returns a newly created UUID
 func New() UUID {
 	return UUID{
 		UUID: guid.New(),
 	}
 }
 
+// String is a wrapper for Google's UUID.String
 func (u UUID) String() string {
 	return u.UUID.String()
 }
